6ec9ced99e2e1ffa005102e23317942a63c67ea1
Palani M <mpalanipsbb@gmail.com>
2023-10-25 23:17:42 +0530

    adding BR for app 2

--- jobs/src/index.js
+++ jobs/src/index.js
@@ -8,9 +8,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
-  </BrowserRouter>
+
 
 
 
